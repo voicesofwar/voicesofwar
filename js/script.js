59f4c9414074673cc3a52f2d187a3518a6fdb2fa
Rana Coskun <coskunrana13@gmail.com>
2023-02-12 19:27:22 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+const text = document.getElementById('myText');
+
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
+
+function changeText() {
+  text.src = '../texts/ita/umberto/umberto1.txt';
+}
+
+function changeText2() {
+  text.src = '../texts/ita/umberto/umberto1_eng.txt';
+}
+
+function changeText3() {
+  text.src = '../texts/ita/umberto/umberto1_rus.txt';
+}
+
+function changeText4() {
+  text.src = '../texts/ita/umberto/umberto1_tur.txt';
+}
+
+function changeText5() {
+  text.src = '../texts/tur/hasan/hasan.txt';
+}
+
+function changeText6() {
+  text.src = '../texts/tur/hasan/hasan_eng.txt';
+}
+
+function changeText7() {
+  text.src = '../texts/tur/hasan/hasan_ita.txt';
+}
+
+function changeText8() {
+  text.src = '../texts/tur/hasan/hasan_rus.txt';
+}
+
+function changeText9() {
+  text.src = '../texts/ita/michele/michele1.txt';
+}
+
+function changeText10() {
+  text.src = '../texts/ita/michele/michele1_eng.txt';
+}
+
+function changeText11() {
+  text.src = '../texts/ita/michele/michele1_rus.txt';
+}
+
+function changeText12() {
+  text.src = '../texts/ita/michele/michele1_tur.txt';
+}
+
+function changeText13() {
+  text.src = '../texts/rus/janis/janis1.txt';
+}
+
+function changeText14() {
+  text.src = '../texts/rus/janis/janis1_eng.txt';
+}
+
+function changeText15() {
+  text.src = '../texts/rus/janis/janis1_ita.txt';
+}
+
+function changeText16() {
+  text.src = '../texts/rus/janis/janis1_tur.txt';
+}
+
+function changeText17() {
+  text.src = '../texts/tur/mehmet/mehmet.txt';
+}
+
+function changeText18() {
+  text.src = '../texts/tur/mehmet/mehmet_eng.txt';
+}
+
+function changeText19() {
+  text.src = '../texts/tur/mehmet/mehmet_ita.txt';
+}
+
+function changeText20() {
+  text.src = '../texts/tur/mehmet/mehmet_rus.txt';
+}
+
+function changeText21() {
+  text.src = '../texts/ita/umberto/umberto2.txt';
+}
+
+function changeText22() {
+  text.src = '../texts/ita/umberto/umberto2_eng.txt';
+}
+
+function changeText23() {
+  text.src = '../texts/ita/umberto/umberto2_rus.txt';
+}
+
+function changeText24() {
+  text.src = '../texts/ita/umberto/umberto2_tur.txt';
+}
+
+function changeText25() {
+  text.src = '../texts/rus/janis/janis2.txt';
+}
+
+function changeText26() {
+  text.src = '../texts/rus/janis/janis2_eng.txt';
+}
+
+function changeText27() {
+  text.src = '../texts/rus/janis/janis1_ita.txt';
+}
+
+function changeText28() {
+  text.src = '../texts/rus/janis/janis1_tur.txt';
+}
+
+function changeText29() {
+  text.src = '../texts/ita/michele/michele2.txt';
+}
+
+function changeText30() {
+  text.src = '../texts/ita/michele/michele2_eng.txt';
+}
+
+function changeText31() {
+  text.src = '../texts/ita/michele/michele1_rus.txt';
+}
+
+function changeText32() {
+  text.src = '../texts/ita/michele/michele2_tur.txt';
+}
+
+function changeText33() {
+  text.src = '../texts/ita/michele/michele3.txt';
+}
+function changeText34() {
+  text.src = '../texts/ita/michele/michele3_eng.txt';
+}
+
+function changeText35() {
+  text.src = '../texts/ita/michele/michele3_rus.txt';
+}
+
+function changeText36() {
+  text.src = '../texts/ita/michele/michele3_tur.txt';
+}
+
+function changeText37() {
+  text.src = '../texts/rus/ivan/ivan.txt';
+}
+function changeText38() {
+  text.src = '../texts/rus/ivan/ivan_eng.txt';
+}
+
+function changeText39() {
+  text.src = '../texts/rus/ivan/ivan_ita.txt';
+}
+
+function changeText40() {
+  text.src = '../texts/rus/ivan/ivan_tur.txt';
+}
+
+function changeText41() {
+  text.src = '../texts/ita/umberto/umberto3.txt';
+}
+
+function changeText42() {
+  text.src = '../texts/ita/umberto/umberto3_eng.txt';
+}
+
+function changeText43() {
+  text.src = '../texts/ita/umberto/umberto3_rus.txt';
+}
+
+function changeText44() {
+  text.src = '../texts/ita/umberto/umberto3_tur.txt';
+}
